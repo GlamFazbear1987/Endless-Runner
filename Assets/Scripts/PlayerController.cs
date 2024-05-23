a14f36c354ac2123e3141de4d56770d58c6bced5
GlamFazbear1987 <146845967+GlamFazbear1987@users.noreply.github.com>
2024-05-23 09:22:32 -0400

    Endless Runner Project Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(new Vector2(0,5), ForceMode2D.Impulse);
+            Debug.Log("Space bar was pressed");
+        }
+    }
+}
